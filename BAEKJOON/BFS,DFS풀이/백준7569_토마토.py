3d3664dac042d0b506d6477c9db8a87ccb32bc23
joyh <whfprl10@gmail.com>
2021-06-06 21:06:14 +0900

    boj7569 풀이

---
+++ BAEKJOON/BFS,DFS풀이/백준7569_토마토.py
@@ -0,0 +1,57 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+# 위, 아래, 왼쪽, 오른쪽, 앞, 뒤
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+m, n, h = map(int, input().split())
+
+# 3차원 배열 생성
+graph = []
+for _ in range(h):
+    g = []
+    for j in range(n):
+        g.append(list(map(int, input().split())))
+    graph.insert(0, g)
+# print("bfs 적용 전",graph) #<=====================
+
+def bfs():
+    while q:
+        a, b, c = q.popleft()
+
+        for i in range(6):
+            x = a + dx[i]
+            y = b + dy[i]
+            z = c + dz[i]
+            if 0 <= x < n and 0 <= y < m and 0 <= z < h and graph[z][x][y] == 0:
+                q.append([x, y, z])
+                graph[z][x][y] = graph[c][a][b] + 1
+
+
+q = deque()
+isTrue = False
+
+for z in range(h):
+    for x in range(n):
+        for y in range(m):
+            if graph[z][x][y] == 1:
+                q.append([x, y, z])
+
+bfs()
+# print("bfs 적용 후", graph) #<=====================
+
+max_num = 0
+for z in range(h):
+    for x in range(n):
+        for y in range(m):
+            if graph[z][x][y] == 0:
+                isTrue = True
+            max_num = max(max_num, graph[z][x][y])
+
+if isTrue == True:
+    print(-1)
+else:
+    print(max_num - 1)
