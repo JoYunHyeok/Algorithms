b0fb5889406510711f80e6dd330833681f46b865
joyh <whfprl10@gmail.com>
2021-09-10 15:40:02 +0900

    백준20040 사이클 게임 풀이 코드 추가

---
+++ BAEKJOON/유니온파인드/백준20040_사이클게임.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+input = stdin.readline
+
+n, m = map(int, input().split())
+parent = [i for i in range(n)]
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] =b
+
+flag = True
+
+for i in range(m):
+    x, y = map(int, input().split())
+
+    if find_parent(parent, x) == find_parent(parent, y):
+        print(i+1)
+        flag = False
+        break
+    else:
+        union_parent(parent, x, y)
+
+if flag:
+    print(0)
