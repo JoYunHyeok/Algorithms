ee729133168aee42e19406f45135b1c0d0f81615
joyh <whfprl10@gmail.com>
2021-09-17 17:07:58 +0900

    프로그래머스 level1 소수찾기 풀이

--- BAEKJOON/백트래킹,BFS,DFS/복습연습장.py
+++ BAEKJOON/백트래킹,BFS,DFS/복습연습장.py
@@ -1,74 +1,7 @@
-from sys import stdin
+from collections import Counter
 
-n = int(input())
+print(Counter('hello world'))
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, stdin.readline().strip())))
-
-cnt = 0
-cnt_list = []
-
-def dfs(x, y):
-    global cnt
-    if x <= -1 or x >= n or y <= -1 or y >= n:
-        return False
-
-    if graph[x][y] == 1:
-        cnt += 1
-        graph[x][y] = 0
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-    return False
-
-result = 0
-for i in range(n):
-    for j in range(n):
-        if dfs(i, j) == True:
-            result += 1
-            cnt_list.append(cnt)
-            cnt = 0
-
-cnt_list.sort()
+array = [('홍길동', 35), ('이순신', 75), ('아무개', 50)]
+result = sorted(array, key=lambda x : x[1], reverse=False)
 print(result)
-print(*cnt_list, sep='\n')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
