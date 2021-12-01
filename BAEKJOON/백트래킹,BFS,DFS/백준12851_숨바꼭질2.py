10ea194b2dd522b560fe8f3c9f7727457a05beff
joyh <whfprl10@gmail.com>
2021-12-01 20:12:49 +0900

    백준12581 숨바꼭질2 풀이코드

---
+++ BAEKJOON/백트래킹,BFS,DFS/백준12851_숨바꼭질2.py
@@ -0,0 +1,33 @@
+from collections import deque
+from sys import stdin
+input = stdin.readline
+MAX = 100000
+n, k = map(int, input().split())
+visited = [False] * (MAX+1)
+graph = [0] * (MAX+1)
+visited[n] = True
+
+def bfs():
+    queue = deque()
+    queue.append(n)
+    cnt = 0
+    while queue:
+        x = queue.popleft()
+
+        if x == k:
+            print(graph[x])
+            cnt += 1
+            print(cnt)
+
+        for dx in [x-1, x+1, x*2]:
+            if 0 <= dx <= MAX:
+                if not visited[dx] or graph[dx] == graph[x] + 1:
+                    queue.append(dx)
+                    visited[dx] = True
+                    graph[dx] = graph[x] + 1
+
+bfs()
+
+# 5 - 10 - 9 - 18 - 17
+# 5 - 4  - 8 - 16 - 17
+
