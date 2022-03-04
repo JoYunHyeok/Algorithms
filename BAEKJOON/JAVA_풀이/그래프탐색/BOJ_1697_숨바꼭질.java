863be91fefe902fca28d2c34027f8f365fc43659
JoYunHyeok <whfprl10@gmail.com>
2022-03-05 01:51:22 +0900

    [BOJ]1697 숨바꼭질/실버1/유용한 풀이

--- BAEKJOON/JAVA_풀이/그래프탐색/BOJ_1697_숨바꼭질.java
+++ BAEKJOON/JAVA_풀이/그래프탐색/BOJ_1697_숨바꼭질.java
@@ -44,3 +44,57 @@ public class BOJ_1697_숨바꼭질 {
 	}
 
 }
+
+//더 유용한 풀이
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_1697_숨바꼭질_세번째풀이 {
+	static int[] dx = { -1, +1, 2 };
+	static int N, K, cnt;
+	static boolean[] visited = new boolean[100001];
+	static int[] map = new int[100001];
+	static Queue<Integer> q = new LinkedList<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+
+		N = sc.nextInt(); // 수빈이
+		K = sc.nextInt(); // 동생
+
+		q.offer(N);
+		visited[N] = true;
+
+		while (!q.isEmpty()) {
+			int curr = q.poll();
+			visited[curr] = true;
+
+			if (curr == K) {
+				System.out.println(map[curr]);
+				break;
+			}
+
+			for (int i = 0; i < dx.length; i++) {
+				int nextV = 0;
+				if (i == 2)
+					nextV = curr * dx[i];
+				else
+					nextV = curr + dx[i];
+
+				if (nextV >= 0 && nextV < 100001) {
+					if (!visited[nextV]) {
+						visited[nextV] = true;
+						q.offer(nextV);
+						map[nextV] = map[curr] + 1; // 어차피 제일 가까운 곳에만 +1을 할테니
+					}
+				}
+			}
+		}
+
+	}
+
+}
+
