0407b6389f8b642f5f738ea600ee64dd94dc9379
조윤혁 <whfprl10@gmail.com>
2022-03-20 03:28:22 +0900

    [BOJ] 팩토리얼/브론즈3/수학

---
+++ BAEKJOON/JAVA_풀이/수학/BOJ_10872_팩토리얼.java
@@ -0,0 +1,20 @@
+package 백준수학;
+
+import java.util.Scanner;
+
+public class BOJ_10872_팩토리얼 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		System.out.println(factorial(N));
+	}
+	static int factorial(int n) {
+		if(n == 0)
+			return 1; 
+		return n * factorial(n-1);
+	}
+
+}
