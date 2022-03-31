2607f94a9652db327cc1a595e499e967fab653ca
조윤혁 <whfprl10@gmail.com>
2022-03-31 17:46:20 +0900

    [BOJ]1로만들기/실버3/DP

--- BAEKJOON/JAVA_풀이/다이나믹프로그래밍/BOJ_1463_1로만들기.java
+++ BAEKJOON/JAVA_풀이/다이나믹프로그래밍/BOJ_1463_1로만들기.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class BOJ_1463_1로만들기 {
 	static int[] D;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		D = new int[N+1];
+		D = new int[N + 1];
 		D[1] = 0;
-		for(int i=2; i<=N; i++	) {
-			int min = Integer.MAX_VALUE;
-			if(i%3==0 && D[i/3] + 1 < min) min = D[i/3] + 1;
-			if(i%2==0 && D[i/2] + 1 < min) min = D[i/2] + 1;
-			if(i>1 && D[i-1] + 1 < min) min = D[i-1] + 1;
-			D[i] = min;
+		for (int i = 2; i <= N; i++) {
+			D[i] = D[i - 1] + 1;
+			if (i % 2 == 0 && D[i / 2] + 1 < D[i]) D[i] = D[i / 2] + 1;
+			if (i % 3 == 0 && D[i / 3] + 1 < D[i]) D[i] = D[i / 3] + 1;
+
 		}
-		
+
 		System.out.println(D[N]);
 	}
 
